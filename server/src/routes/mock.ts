8d062e8f906a5d04fd9413a421bc1db375cc1f2d
Ines R <igvoic.work@gmail.com>
2023-12-13 20:56:33 +0100

    Revert "Ir/#5/create db and table"

--- server/src/routes/mock.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { mockController } from "../controllers/mock";
-
-
-const mockRoutes = Router();
-
-mockRoutes.get("/:type", mockController.mockData);
-
-export default mockRoutes;
